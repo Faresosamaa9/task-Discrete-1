f246b90d5af7e26ad31e4870dbc3fccd668e7ba9
Faresosamaa9 <120976143+Faresosamaa9@users.noreply.github.com>
2022-12-29 15:46:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine(" please enter string number ");
+            int strtNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(" please enter eding number ");
+            int endNum  = int.Parse(Console.ReadLine());
+            Console.WriteLine("These are the number between {0} and {1} " , strtNum, endNum);
+            for (int i = strtNum; i <= endNum; i++)
+            {
+                int counter = 0;
+                for (int j = 2; j <= i/2; j++)
+                {
+                    if (i % j == 0)
+                        counter++;
+                    break;
+                }
+                if (counter == 0 && i !=1)
+                    Console.WriteLine(" {0} ", i);
+            }
+
+        }
+    }
+}
